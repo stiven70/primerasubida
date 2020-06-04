e41246fea89d714b52f18e6662c578edb3a0b682
stiven ruiz <stiven70@outlook.com>
2020-06-04 09:34:35 -0500

    ya no los necesito

--- index.js
+++
@@ -1,9 +0,0 @@
-const { obtenerPromedio, argv} = require('./datos.js');
-
-console.log(argv);
-if(argv._[0]=='promedio'){
-    console.log('El promedio de ' + argv.n + 'es '+ obtenerPromedio);
-}
-else{
-    console.log(' El promedio no calculado ');
-}
